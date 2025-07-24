f1811e191988a4136052efdefd6f075f88ae51ed
Xuncheng Wang <w.xun.cheng@gmail.com>
2025-07-24 17:29:51 +0800

    chore(nvim): adjust keymaps, refresh plugin lock

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local map = vim.keymap.set
+
 -- <leader><leader> switches to the previous buffer
-vim.keymap.set("n", "<leader><leader>", "<C-^>", { noremap = true, silent = true })
+map("n", "<leader><leader>", "<C-^>", { noremap = true, silent = true })
 
 -- Enter clears the search highlight
-vim.keymap.set("n", "<CR>", ":nohlsearch<CR>:<CR>", { noremap = true, silent = true })
+map("n", "<CR>", ":nohlsearch<CR>:<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("i", "<C-l>", "<space>=><space>", {})
+map("i", "<C-l>", "<space>=><space>", {})
 
 -- <leader>n renames the file
 function RenameFile()
@@ -19,7 +21,7 @@ function RenameFile()
     vim.fn.execute(":e " .. new_name)
   end
 end
-vim.keymap.set("n", "<leader>n", "<cmd>lua RenameFile()<CR>", {})
+map("n", "<leader>n", "<cmd>lua RenameFile()<CR>", {})
 
 -- local Util = require("lazyvim.util")
 -- vim.keymap.set("n", "<leader>t", function()
@@ -27,8 +29,14 @@ vim.keymap.set("n", "<leader>n", "<cmd>lua RenameFile()<CR>", {})
 -- end, { desc = "Terminal (cwd)" })
 
 -- https://github.com/LazyVim/LazyVim/discussions/4109
-vim.keymap.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<CR>")
-vim.keymap.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<CR>")
-vim.keymap.set("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<CR>")
-vim.keymap.set("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<CR>")
-vim.keymap.set("n", "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<CR>")
+map("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<CR>")
+map("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<CR>")
+map("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<CR>")
+map("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<CR>")
+map("n", "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<CR>")
+
+-- Override LazyVim's default keymaps
+-- map("n", "<leader>e", function() Snacks.explorer() end, { desc = "Explorer Snacks (cwd)" })
+-- map("n", "<leader>E", function() Snacks.explorer({ cwd = LazyVim.root() }) end, { desc = "Explorer Snacks (root dir)" })
+-- map("n", "<leader>ff", function() Snacks.picker.files({ root = false }) end, { desc = "Find Files (cwd)" })
+-- map("n", "<leader>fF", function() Snacks.picker.files() end, { desc = "Find Files (root Dir)" })
